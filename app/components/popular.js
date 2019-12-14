e7507694688493f851b5384c30146712d2a7ecdf
Taco <taco-tues@tuta.io>
2019-12-14 08:51:50 -0700

    remove styles from popular then add to results

--- app/components/popular.js
+++ app/components/popular.js
@@ -5,27 +5,6 @@ import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons
 import Loading from './Loading'
 import Card from './Card'
 
-const styles = {
-  container: {
-    position: 'relative',
-    display: 'flex'
-  },
-  tooltip: {
-    boxSizing: 'border-box',
-    position: 'absolute',
-    width: '160px',
-    bottom: '100%',
-    left: '50%',
-    marginLeft: '-80%px',
-    borderRadius: '3px',
-    backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
-    padding: '7px',
-    marginBottom: '5px',
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: '14px',
-  }
-}
 function LanguagesNav ({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
   
