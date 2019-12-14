e7149f67548b940c6547f7337cc02b0ff419b9ef
Taco <taco-tues@tuta.io>
2019-12-14 15:31:31 -0700

    add ThemeProvider

--- app/index.js
+++ app/index.js
@@ -3,15 +3,30 @@ import ReactDOM from 'react-dom'
 import './index.css' 
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
 
 
 class App extends React.Component {
+  constructor() {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
   render() {
     return (
-      <div className='container'>
-        <Battle />
-        {/* <Popular /> */}
-      </div>
+      <ThemeProvider value={}>
+        <div className='container'>
+          <Battle />
+          {/* <Popular /> */}
+        </div>
+      </ThemeProvider>
     )
   }
 }
