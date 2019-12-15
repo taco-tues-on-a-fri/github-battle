0b28b66d2931943b56fe6410ef99e37e67d48b89
Taco <taco-tues@tuta.io>
2019-12-15 10:38:02 -0700

    add exact to '/' route

--- app/index.js
+++ app/index.js
@@ -28,7 +28,7 @@ class App extends React.Component {
             <div className='container'>
               <Nav />
 
-              <Route path='/' component={Popular} />
+              <Route exact path='/' component={Popular} />
               <Route path='/battle' component={Battle} />
             </div>
           </div>
